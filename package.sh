7410292ee1dd30317e82f79aec9c51ed99c43cb6
Tim Harper <timcharper@gmail.com>
2008-08-26 16:07:16 -0600

    updated package.sh script

--- package.sh
+++ package.sh
@@ -4,7 +4,6 @@ cd `dirname "$0"`
 VERSION=`git tag | sort_rev | tail -n 1`
 rm -rf textile
 git archive --prefix=textile/ $VERSION | tar xf -
-cd textile
-rm .gitignore
-rm package.sh
+rm textile/.gitignore textile/package.sh
 
+zip -r textile-$VERSION.zip textile
